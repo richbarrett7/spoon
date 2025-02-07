5545a4496202dfa4d8f61ffc0c245628fdf1a1f9
richbarrett7 <rich@parcelmonkey.com>
2025-02-07 11:33:15 +0000

    WIP

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: {
+    relative: true,
+    files: [
+      './public/**/*.html',
+      './public/assets/js/src/**/*.js',
+    ]
+  },
+  theme: {
+    extend: {
+      colors: {
+        'spoon-yellow': '#ffc762',
+        'spoon-blue': '#28d2d1',
+        'spoon-orange': '#ff7f4c',
+        'spoon-burnt-orange': '#e49e6e',
+        'spoon-dark-blue': '#549ed6',
+        'spoon-green': '#35ce7f',
+        'spoon-pale-yellow': '#f4e0a6', 
+        'spoon-alt-blue': '#00b3e4'
+      },
+    },
+  },
+  plugins: [],
+  darkMode: 'selector',
+}
